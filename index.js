d44860ec1088de6a6992f3b8eb923f13ba340c9b
Petar D. Petrov <102607959+petroffD21@users.noreply.github.com>
2023-02-17 23:54:57 +0200

    Created keyboard input, character movement even though its unfinished, created pacman

--- index.js
+++ index.js
@@ -1,25 +1,65 @@
+// Map
 let canvas = document.querySelector('canvas')
 let context = canvas.getContext('2d')
 
 let image = new Image()
 image.src = 'spritesheet.png'
-image.crossOrigin = true;
 
 image.onload = () => {
-    canvas.width = 680;
-    canvas.height = 248;
-    
+    context.clearRect(-450, 0, canvas.width, canvas.height)
     context.drawImage(
         image,
-        -450, 
+        -450,
         0, // whole spritesheet x and y offset positions
         image.width,
         image.height, // width and height of the source image
-        0,
-        0, // desired offset to isolate the map
+        -450,
+        0, // offset
         canvas.width,
         canvas.height
-    )
+    ) //map
+    
+
 }
 
+
+
 //todo keyboard input i pacman da si smenq sprite sheet kartinkata spored inputa/posokata
+
+//Keyboard Input and character
+let character = document.getElementById('pacmanRight')
+character.src = 'pacman.png'
+character.width = 30
+character.height = 30
+let characterX = -450;
+let characterY = 0;
+
+
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'w':
+            //character.source = pacmannagore.png primerno
+            characterX += 0
+            characterY += 2
+            character.style.transform = 'translate(' + characterX + 'px, ' + characterY + 'px)'
+            break;
+        case 'd':
+            characterX += 5
+            characterY += 0
+            break;
+        case 'a':
+            characterX -= 5
+            characterY += 0
+            break;
+        case 's':
+            characterX += 0
+            characterY -= 2
+    }
+    console.log(e);
+    console.log(characterX, characterY);
+
+})
+
+
+
+
