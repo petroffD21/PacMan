37696c1589c0f0c3d699400f63cf96f3385412a7
Petar D. Petrov <102607959+petroffD21@users.noreply.github.com>
2023-02-18 12:07:39 +0200

    Better movement
    Improved a bit pacman's movement

--- index.js
+++ index.js
@@ -18,13 +18,16 @@ image.onload = () => {
         canvas.width,
         canvas.height
     ) //map
-    
+
 
 }
 
-
-
 //todo keyboard input i pacman da si smenq sprite sheet kartinkata spored inputa/posokata
+
+// Click listener to get mouse coordinates
+canvas.addEventListener('click', e => {
+    console.log('X: ', e.clientX, 'Y: ', e.clientY);
+})
 
 //Keyboard Input and character
 let character = document.getElementById('pacmanRight')
@@ -34,26 +37,29 @@ character.height = 30
 let characterX = -450;
 let characterY = 0;
 
-
 window.addEventListener('keydown', (e) => {
     switch (e.key) {
         case 'w':
             //character.source = pacmannagore.png primerno
             characterX += 0
-            characterY += 2
+            characterY -= 2
             character.style.transform = 'translate(' + characterX + 'px, ' + characterY + 'px)'
             break;
         case 'd':
             characterX += 5
             characterY += 0
+            character.style.transform = 'translate(' + characterX + 'px, ' + characterY + 'px)'
             break;
         case 'a':
             characterX -= 5
             characterY += 0
+            character.style.transform = 'translate(' + characterX + 'px, ' + characterY + 'px)'
             break;
         case 's':
             characterX += 0
-            characterY -= 2
+            characterY += 2
+            character.style.transform = 'translate(' + characterX + 'px, ' + characterY + 'px)'
+            break;
     }
     console.log(e);
     console.log(characterX, characterY);
