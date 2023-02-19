010c1fd54f54777267d20a4cf438f102ec822e09
Petar D. Petrov <102607959+petroffD21@users.noreply.github.com>
2023-02-19 13:22:19 +0200

    .

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Map
 let canvas = document.querySelector('canvas')
 let context = canvas.getContext('2d')
 
@@ -11,62 +10,28 @@ image.onload = () => {
         0,
         0,
         225,
-        image.height, 
+        image.height,
         0,
-        0, // offset
+        0,
         canvas.width,
         canvas.height
     ) //map
-    
+
     context.drawImage(
         image,
         230,
-        100, //TODO SLED MALKO DA GO CLIPNA SPORED HEIGHTA
-        450,
-        450,
-        50,
         0,
-        canvas.width,
-        canvas.height
-        
-    ) // pacman sprite
-    // let pacman = {
-    //     x: 200,
-    //     y: 200,
-    //     speed: 5,
-    //     direction: 'right',
-    //     mouthOpen: true,
-    //     radius: 20,
-    
-    //     draw: function() {
-    //         context.drawImage(
-    //             image,
-    //             0,
-    //             0,
-    //             100,
-    //             this.x,
-    //             this.y,
-    //             canvas.width * 0.3,
-    //             canvas.height * 0.3
-    //         )
-    //     },
-    
-    //     move: function() {
-    //         switch(this.direction) {
-    //             case 'up':
-    //                 this.y -= this.speed;
-    //                 break;
-    //             case 'down':
-    //                 this.y += this.speed;
-    //                 break;
-    //             case 'left':
-    //                 this.x -= this.speed;
-    //                 break;
-    //             case 'right':
-    //                 this.x += this.speed;
-    //                 break;
-    //         }
-    
+        15.5,
+        15.5,
+        50,
+        50,
+        canvas.width * 0.06,
+        canvas.height * 0.06
+    ) // pacman
+
+
+
+
     //         // Collision check
     //         if (this.x - this.radius < 0 || this.x + this.radius > canvas.width || this.y - this.radius < 0 || this.y + this.radius > canvas.height) {
     //             this.x = 320;
@@ -80,11 +45,8 @@ image.onload = () => {
     //             }
     //           }
     //     }
-        
-    
-    // }
-    // pacman.draw()
-    // pacman.move()
+
+
 }
 
 
